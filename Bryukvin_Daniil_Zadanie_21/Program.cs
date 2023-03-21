a44789a7712fa565855172af5345d11e45e33c7b
Daniil Bryukvin <mif74012@gmail.com>
2023-03-21 18:50:42 +0300

    Seminar_3_dz

---
+++ Bryukvin_Daniil_Zadanie_21/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите посследовательность координат: ");
+int XA, YA, ZA;
+int XB, YB, ZB;
+
+    Console.Write("Введите X точки A: ");
+XA = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Y точки A: ");
+YA = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Z точки A: ");
+ZA = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите X точки B: ");
+XB = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Y точки B: ");
+YB = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите Z точки B: ");
+ZB = Convert.ToInt32(Console.ReadLine());
+
+double number = Math.Pow(XB - XA, 2) + Math.Pow(YB - YA, 2) + Math.Pow(ZB - ZA, 2);
+number = Math.Sqrt(number);
+number = Math.Round(number, 2);
+Console.WriteLine(number);
